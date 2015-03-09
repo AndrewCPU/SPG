e97371a106ee9ce896c1caba432a287525ee27cc
monkeyboystein <stein.andrew.01@gmail.com>
2015-03-09 15:28:48 -0400

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -4,6 +4,47 @@
 public class Tile {
     int worldX;
     int worldY;
+    BlockType type;
+    boolean solid = false;
+    
+    public void setWorldX(int worldX)
+    {
+        this.worldX = worldX;
+    }
+    public void setWorldY(int worldY)
+    {
+        this.worldY = worldY;
+    }
+    
+    public int getWorldX()
+    {
+        return worldX;
+    }
+    public int getWorldY()
+    {
+        return worldY;
+    }
+    
+    public BlockType getType()
+    {
+        return type;
+    }
+    public void setType(BlockType type)
+    {
+        this.type = type;
+    }
+    
+    public boolean isSolid()
+    {
+        return solid;
+    }
+    public void setSolid(boolean solid)
+    {
+        this.solid = solid;
+    }
+    
+    
+    
 
 
 
